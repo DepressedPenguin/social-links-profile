04262cb3ff3becc0bb9af91d8ad60006e8e80787
DepressedPenguin <125665615+DepressedPenguin@users.noreply.github.com>
2024-01-22 15:34:19 +0100

    Added Necessary Files from my desktop

---
+++ script_links.js
@@ -0,0 +1,28 @@
+
+// GITHUB
+document.getElementById("githubBtn").onclick = function() {
+    window.open("https://github.com/DepressedPenguin", "_blank");
+};
+
+// linkedin
+document.getElementById("linkBtn").onclick = function() {
+    window.open("https://www.linkedin.com/in/zakaria-elaroussi-609121223/", "_blank");
+};
+
+
+//Frontend
+
+document.getElementById("mentorBtn").onclick = function() {
+    window.open("https://www.frontendmentor.io/profile/DepressedPenguin", "_blank");
+};
+
+//Twittetr
+document.getElementById("twitterBtn").onclick = function() {
+    window.open("https://twitter.com/DP_0_PENGUIN", "_blank");
+};
+
+//Instagram
+document.getElementById("instagramBtn").onclick = function() {
+    window.open("https://www.instagram.com/ignite_z02/", "_blank");
+};
+
